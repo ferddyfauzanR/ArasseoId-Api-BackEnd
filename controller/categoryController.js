08ed7b1749a9c9b667ae84e355de5690a23f055f
Ferddy Fauzan <ferddyfauzan@gmail.com>
2024-01-28 01:33:55 +0700

    Fixing Update Category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,7 +1,7 @@
-const {Category} = require("../models")
+const { Category } = require("../models")
 
 
-const getCategory = async(req,res) =>{
+const getCategory = async (req, res) => {
     try {
         const response = await Category.findAll();
         const result = {
@@ -10,40 +10,40 @@ const getCategory = async(req,res) =>{
         }
         res.json(result)
     } catch (error) {
-        res.status(500).json({msg:error.message })
+        res.status(500).json({ msg: error.message })
     }
 }
 
 const getCategoryById = async (req, res) => {
     try {
-        const {id} = req.params
+        const { id } = req.params
         const data = await Category.findByPk(id)
-        if(data === null){
+        if (data === null) {
             return res.status(404).json({
-                status:"failed",
+                status: "failed",
                 message: `data Category With ID ${id} is not Found!!!`
             })
         }
         res.json({
-            status:"OK",
-            data:data
+            status: "OK",
+            data: data
         })
     } catch (error) {
-        res.status(500).json({msg : error.message})
+        res.status(500).json({ msg: error.message })
     }
 }
 
-const createCategory = async (req,res) =>{
+const createCategory = async (req, res) => {
     try {
         const { name } = req.body;
-        const newCategory = await Category.create({name:name});
+        const newCategory = await Category.create({ name: name });
         res.status(201).json({
-            status:"OK",
-            data:{
-                id:newCategory.id,
-                name:newCategory.name,
-                createdAt:newCategory.createdAt,
-                updatedAt:newCategory.updatedAt
+            status: "OK",
+            data: {
+                id: newCategory.id,
+                name: newCategory.name,
+                createdAt: newCategory.createdAt,
+                updatedAt: newCategory.updatedAt
 
             }
         })
@@ -52,33 +52,46 @@ const createCategory = async (req,res) =>{
     }
 }
 
-const updateCategory = async(req,res) =>{
+const updateCategory = async (req, res) => {
     try {
-       await Category.update(req.body,{
-        where:{
-            id:req.params.id
-        }
-       })
-       res.status(200).json({msg: `Update Category by ID  Success...`});
+        const { id } = req.params
+        const data = await Category.findByPk(id);
+        await Category.update(req.body,{
+            where:{
+                id: req.params.id
+            }
+        })
+        if (data === null) {
+            return res.status(404).json({
+                status: "failed",
+                message: "Data Id Not Found"
+            })
 
+        }
+        res.status(200).json({
+            status:"Updated",
+            msg: "Category Updated Success!!",
+            data:data
+            
+        });
     } catch (error) {
         res.status(400).json({ message: error.message });
-        
+
     }
 }
 
-const deleteCategory = async(req,res)=>{
+const deleteCategory = async (req, res) => {
     try {
         await Category.destroy({
-            where:{
-                id:req.params.id
+            where: {
+                id: req.params.id
             }
         })
-        res.status(200).json({msg:`Delete Category by ID success...`})
+        res.status(200).json({ msg: `Delete Category by ID success...` })
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 }
 
 
-module.exports = {getCategory, getCategoryById, createCategory,updateCategory,deleteCategory};
+module.exports = { getCategory, getCategoryById, createCategory, updateCategory, deleteCategory };
